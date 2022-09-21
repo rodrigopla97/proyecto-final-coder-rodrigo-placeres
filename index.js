4699aeca49af018335d7b8f9b8732872488e54db
rodrigopla97 <rodrigoplaceres19@gmail.com>
2022-09-21 19:06:20 -0300

    Segunda entrega - Proyecto final Coder

--- index.js
+++ index.js
@@ -1,179 +1,166 @@
-let descuentoPrecio = (num) => {
-    return num - 1000;
-};
-
-let descuentoEfectivo = (num) => num - num * 0.1;
-
-function filterArray(value) {
-    return compra.filtroCarrito.push(`${value.nombre} ${value.tipo}`);
+function innerVacio() {
+    carritoVacio.innerHTML = "";
+    ulList.innerHTML = "";
+    pTotal.innerText = "";
+    pSaludo.innerText = "";
 }
-
-class Producto {
-    constructor(nombre, tipo, precio) {
+class usuario {
+    constructor(id, nombre, tipo, precio) {
+        this.id = id;
         this.nombre = nombre;
         this.tipo = tipo;
         this.precio = precio;
     }
 }
-
+class Producto {
+    constructor(id, nombre, tipo, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.precio = precio;
+    }
+}
 class Compra {
     constructor(cuotas) {
         this.totalCompra = 0;
         this.cuotas = cuotas;
         this.carrito = [];
         this.filtroCarrito = [];
-        this.error = "######. No ha ingresado una opción válida, vuelva a empezar ya que sa terminó su posibilidad de compra";
-    }
-    recargoTarjeta() {
-
-        if (this.cuotas === 1) {
-            return this.totalCompra;
-        } else if (this.cuotas === 2) {
-            return this.totalCompra * 1.1;
-        } else if (this.cuotas === 3) {
-            return this.totalCompra * 1.25;
-        } else {
-            return this.error;
-        }
     }
 }
 
-let tipoPantalon;
-let tipoRemera;
-let tipoCalzado;
+const btnAgregar = document.createElement("button");
+const btnFinalizar = document.createElement("button");
+const btnCancelar = document.createElement("button");
+const btnVaciar = document.createElement("button");
+const pFooter = document.createElement("p");
+const tituloUsuario = document.getElementById("tituloUsuario");
+const formUsuario = document.getElementById("formUsuario");
+const selectTag = document.getElementById("select-productos");
+const carritoVacio = document.getElementById("vacio");
+const carritoContainer = document.getElementById("carrito-container");
+const ulList = document.getElementById("lista");
+const saludo = document.getElementById("saludo");
+const footer = document.getElementById("footer");
+const disponible = document.getElementById("stock");
+const botones = document.getElementById("botones");
+const total = document.getElementById("total");
+const pTotal = document.createElement("p");
+const pSaludo = document.createElement("p");
 
-let seguirComprando = true;
-let compraCancelada = false;
-let decision;
+let productos = [];
+let totalCompra = 0;
+const date = new Date();
+const anio = date.getFullYear();
+let compra = new Compra();
+let nombreUsuario = "";
+let apellidoUsuario = "";
+let correo = "";
 
-let tipoPago;
-let precioFinal = 0;
+botones.append(btnAgregar);
+botones.append(btnFinalizar);
+botones.append(btnCancelar);
+botones.append(btnVaciar);
+footer.append(pFooter);
 
-let compra = new Compra()
+const pantalonUrban = new Producto(1, "Pantalón", "Urban", 2500);
+const pantalonSport = new Producto(2, "Pantalón", "Sport", 2000);
+const remeraUrban = new Producto(3, "Remera", "Urban", 1200);
+const remeraSport = new Producto(4, "Remera", "Sport", 1500);
+const calzadoUrban = new Producto(5, "Calzado", "Urban", 6000);
+const calzadoSport = new Producto(6, "Calzado", "Sport", 8000);
 
-const pantalonUrban = new Producto("Pantalón", "Urban", 2500);
-const pantalonSport = new Producto("Pantalón", "Sport", 2000);
-const remeraUrban = new Producto("Remera", "Urban", 1200);
-const remeraSport = new Producto("Remera", "Sport", 1500);
-const calzadoUrban = new Producto("Calzado", "Urban", 6000);
-const calzadoSport = new Producto("Calzado", "Sport", 8000);
+productos.push(pantalonUrban, pantalonSport, remeraUrban, remeraSport, calzadoUrban, calzadoSport);
 
-let nombreCliente = prompt("Ingrese su nombre: ").toUpperCase();
-alert(`Bienvenido ${nombreCliente}!, a continuacion podrás seleccionar que producto deseas cargar al carrito`);
+productos.forEach((producto) => {
+    const option = document.createElement("option");
+    option.innerText = `${producto.nombre} ${producto.tipo}: $${producto.precio}`;
+    option.setAttribute("id", `${producto.id}`);
+    option.setAttribute("class", "option");
+    selectTag.append(option);
+});
 
-let productoSeleccionado = parseInt(prompt("1.Pantalones - 2.Remeras - 3.Calzado - 4.Cancelar compra"));
+btnAgregar.innerText = "Agregar Producto";
+btnFinalizar.innerText = "Finalizar Compra";
+btnCancelar.innerText = "Cancelar Compra";
+btnVaciar.innerText = "Vaciar Carrito";
+carritoVacio.innerText = `Carrito vacío :c`;
+pFooter.innerText = `Rodrigo Placeres ${anio}`;
 
-while (seguirComprando === true) {
-    if (productoSeleccionado === 1) {
-        tipoPantalon = parseInt(prompt("Ingrese tipo de pantalón: 1.Urban - 2.Sport"));
-
-        if (tipoPantalon === 1) {
-            compra.carrito.push(pantalonUrban);
-        }
-        else if (tipoPantalon === 2) {
-            compra.carrito.push(pantalonSport);
-        }
-        else {
-            alert("OPCIÓN INVÁLIDA!");
-            continue;
-        }
+formUsuario.onsubmit = (e) => {
+    e.preventDefault();
+    const infoUserArray = [];
+    for (const element of e.target.children) {
+        const usuarioObj = {};
+        usuarioObj["tipo"] = element.name;
+        usuarioObj["valor"] = element.value;
+        infoUserArray.push(usuarioObj);
     }
-    else if (productoSeleccionado === 2) {
-        tipoRemera = parseInt(prompt("Ingrese tipo de remera: 1.Urban - 2.Sport"));
+    localStorage.setItem("info", JSON.stringify(infoUserArray));
 
-        if (tipoRemera === 1) {
-            compra.carrito.push(remeraUrban);
-        }
-        else if (tipoRemera === 2) {
-            compra.carrito.push(remeraSport);
-        }
-        else {
-            alert("OPCIÓN INVÁLIDA!");
-            continue;
-        }
-    }
-    else if (productoSeleccionado === 3) {
-        tipoCalzado = parseInt(prompt("Ingrese tipo de calzado: 1.Urban - 2.Sport"));
+    window.location.reload();
+};
 
-        if (tipoCalzado === 1) {
-            compra.carrito.push(calzadoUrban);
-        }
-        else if (tipoCalzado === 2) {
-            compra.carrito.push(calzadoSport);
-        }
-        else {
-            alert("OPCIÓN INVÁLIDA!");
-            continue;
-        }
-    }
-    else if (productoSeleccionado === 4) {
-        alert("Que lastima! Te esperamos la proxima!");
-        compra.totalCompra = 0;
-        seguirComprando = false;
-        compraCancelada = true;
-        break;
-    }
-    else {
-        alert("PRODUCTO SELECCIONADO NO EXISTE! Ingrese un producto válido para cargar en el carrito:");
-        productoSeleccionado = parseInt(prompt("1.Pantalones - 2.Remeras - 3.Calzado - 4.Cancelar compra"));
-        continue;
-    }
+if (localStorage.length > 0) {
+    const info = JSON.parse(localStorage.getItem("info"));
 
-    let decision = parseInt(prompt("1.Seguir comprando - 2.Finalizar compra - 3.Cancelar compra"));
+    info.forEach((dato) => {
+        if (dato.tipo === "name") {
+            nombre = dato.valor;
+        }
+        if (dato.tipo === "lastname") {
+            apellido = dato.valor;
+        }
+        if (dato.tipo === "email") {
+            correo = dato.valor;
+        }
+    });
 
-    if (decision === 1) {
-        productoSeleccionado = parseInt(prompt("1.Pantalones - 2.Remeras - 3.Calzado - 4.Cancelar compra"));
-    }
-    else if (decision === 2) {
-        seguirComprando = false;
-        break;
-    }
-    else if (decision === 3) {
-        alert("Que lastima! Te esperamos la proxima!");
-        compra.totalCompra = 0;
-        compraCancelada = true;
-        break;
-    }
-    else {
-        decision = parseInt(prompt("OPCIÓN SELECCIONADA NO EXISTE! Ingrese una opcion válida: 1.Seguir comprando - 2.Finalizar compra - 3.Cancelar compra"));
-        continue;
+    if (nombre !== "" && apellido !== "" && correo !== "") {
+        tituloUsuario.innerText = `Hola ${nombre} ${apellido}! Bienvenido a CrowShop`;
+        formUsuario.innerText = "";
+    } else {
+        tituloUsuario.innerText = `No ingresó todos los datos`;
     }
 }
 
-if (compraCancelada !== true) {
-    for (let i = 0; i < compra.carrito.length; i++) {
-        compra.totalCompra = compra.totalCompra + compra.carrito[i].precio;
+btnAgregar.onclick = () => {
+    const productoIngresado = productos[selectTag.selectedIndex];
+    const li = document.createElement("li");
+
+    compra.carrito.push(productoIngresado);
+    compra.carrito.forEach((producto) => (li.innerText = `${producto.nombre} ${producto.tipo}`));
+    ulList.append(li);
+    carritoContainer.append(ulList);
+    carritoVacio.innerHTML = "";
+    pTotal.innerText = "";
+    pSaludo.innerText = "";
+};
+
+btnFinalizar.onclick = () => {
+    totalCompra = 0;
+    compra.carrito.forEach((producto) => (totalCompra = totalCompra + producto.precio));
+    innerVacio();
+
+    if (totalCompra === 0) {
+        carritoVacio.innerText = `Carrito vacío :c`;
+    } else {
+        pTotal.innerText = `El total de la compra es de $${totalCompra}`;
+        total.append(pTotal);
+        compra.carrito = [];
     }
-} else {
-    compra.carrito.splice(0, compra.carrito.length);
-}
+};
 
-if (compra.totalCompra != 0) {
+btnCancelar.onclick = () => {
+    innerVacio();
+    carritoVacio.innerText = `Carrito vacío :c`;
+    pSaludo.innerText = `Que lastima! Te esperamos la proxima!`;
+    saludo.append(pSaludo);
+    compra.carrito = [];
+};
 
-    alert("El total sin descuento de su de su carrito es de ARS$" + compra.totalCompra);
-
-    if (compra.totalCompra > 9000) {
-        precioFinal = descuentoPrecio(compra.totalCompra);
-        alert("El precio por ser compra mayor a ARS$9000 de su carrito es de ARS$" + precioFinal);
-    }
-    else {
-        precioFinal = compra.totalCompra;
-        alert("El precio final de su carrito es de ARS$" + compra.totalCompra);
-    }
-
-    tipoPago = parseInt(prompt("Ingrese tipo de pago: 1.Efectivo - 2.Cuotas"));
-
-    if (tipoPago === 1) {
-        alert("El precio final de su carrito es de ARS$" + descuentoEfectivo(precioFinal));
-    }
-    else if (tipoPago === 2) {
-        compra.cuotas = parseInt(prompt("Elija cantidad de cuotas: 1.Una cuota - 2.Dos cuotas 3.Tres cuotas"));
-        alert("El precio final de su carrito es de ARS$" + compra.recargoTarjeta());
-    }
-
-    console.log(compra.carrito);
-    compra.carrito.filter(filterArray);
-    console.log(compra.filtroCarrito);
-    alert("Los productos incorporados en tu carrito son: " + compra.filtroCarrito.join(' - '));
-
-}
+btnVaciar.onclick = () => {
+    innerVacio();
+    carritoVacio.innerText = `Carrito vacío :c`;
+    compra.carrito = [];
+};
